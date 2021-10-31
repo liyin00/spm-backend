cce1585c33eb5ad87f339884999a6950411c116a
liyin00 <73151500+liyin00@users.noreply.github.com>
2021-11-01 00:12:28 +0800

    SPMG6T9-75-delete-wsgi
    Hi guys, WSGI servers didnt seem to do the job. Deleting it now in case it is the one messing our code up

--- lms/lms.py
+++ lms/lms.py
@@ -830,4 +830,4 @@ def update_by_quizId():
 #End of Quiz Crud ----------------------------------
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
